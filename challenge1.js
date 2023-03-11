cf0f500d970d71086f8a1f4ad5065f922248f0d1
pumchanpom <pum.chanpom@gmail.com>
2023-03-11 12:44:26 +0700

    Create JS for Challenge #1

---
+++ challenge1.js
@@ -0,0 +1,7 @@
+// Challenge #1 Age Calculator
+
+let birthYear = 1989; // store birth year
+let futureYear = 2024; // store future year
+let yourAge = futureYear - birthYear;
+
+console.log(`You were born in ${birthYear}. You will be ${yourAge} in ${futureYear}.`)
